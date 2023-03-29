63a166e884ba2cda907be7dc1e3b923455345eb9
Mativas <76075521+MatiasVasquezOrrego@users.noreply.github.com>
2023-03-29 01:16:56 -0300

    add: app  streamlit chat with dummy alpaca response

---
+++ src/app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from streamlit_chat import message as st_message
+
+def generate_alpaca_message(user_input: str):
+    return "alpaca response"
+
+def generate_chat():
+    user_message = st.session_state.input_text
+    alpaca_message = generate_alpaca_message(user_input=user_message)
+
+    st.session_state.history.append(
+        {'message': user_message, 'is_user': True}
+    )
+
+    st.session_state.history.append(
+        {'message': alpaca_message, 'is_user': False}
+    )
+
+st.title("Alpaca Chat")
+
+if "history" not in st.session_state:
+    history = [
+        {
+            "message": "Hello my friend",
+            "is_user": False
+        }
+    ]
+    st.session_state.history = history
+
+st.text_input("", key="input_text", on_change=generate_chat)
+
+for i in range(len(st.session_state.history)-1, -1, -1):
+    st_message(**st.session_state.history[i], key=str(i))
